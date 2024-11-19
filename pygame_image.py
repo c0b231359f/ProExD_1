601ed0557bf779e71fc6475d74130a7ca93e7aff
kanato3256 <c0b231359f@edu.teu.ac.jp>
2024-11-19 16:30:23 +0900

    こうかとんが上下左右運動できるようになったよ！

--- pygame_image.py
+++ pygame_image.py
@@ -11,27 +11,52 @@ def main():
     screen = pg.display.set_mode((800, 600))#画面の大きさ
 
     clock  = pg.time.Clock()
-    clock.tick(200)#フレームレートの設定 変更_問題５
+    # clock.tick(200)#フレームレートの設定 変更_問題５
 
     #画像の読み込み
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    rgb_img = pg.transform.flip(bg_img, True, False)
 
     bard_img = pg.image.load("fig/3.png") #変更_問２
     bard_img = pg.transform.flip(bard_img, True, False)
+
     tmr = 0
-    x = 0
+    dis = 1 #移動距離
+
+    bard_rect = bard_img.get_rect()
+    bard_rect.center = 300, 200
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x -= 100
-        if x <= -800:
-            x = 0
-        screen.blit(bg_img, [x, 0]) #背景の貼り付け
-        screen.blit(bard_img, [300, 200])
+        
+        #背景画像のループ
+        tmr -= 10
+        x = 0
+        y = 0
+        if tmr <= -3200:
+            tmr = 0
+        screen.blit(bg_img, [tmr, 0]) #背景の貼り付け
+        screen.blit(rgb_img, [tmr+1600, 0]) #背景の貼り付け
+        screen.blit(bg_img, [tmr+3200, 0]) #背景の貼り付け
+
+        #こうかとんの移動
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            y = -dis
+        if key_lst[pg.K_DOWN]:
+            y = dis
+        if key_lst[pg.K_RIGHT]:
+            x = dis
+        if key_lst[pg.K_LEFT]:
+            x = -dis
+        bard_rect.move_ip((x, y))
+        screen.blit(bard_img, bard_rect)
+
+        # screen.blit(bard_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
