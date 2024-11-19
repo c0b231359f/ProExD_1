6d6c4105137b3a6d4cc0c01acc72fa1eba196c66
kanato3256 <c0b231359f@edu.teu.ac.jp>
2024-11-19 15:30:08 +0900

    練習1-5

--- pygame_image.py
+++ pygame_image.py
@@ -4,20 +4,25 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
+    #ゲームの初期化
     pg.init()
-    pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    pg.display.set_caption("はばたけ！こうかとん")#上部バーの名前
+    screen = pg.display.set_mode((800, 600))#画面の大きさ
+
     clock  = pg.time.Clock()
+    clock.tick(200)#フレームレートの設定 変更_問題５
+
+    #画像の読み込み
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    c_img = pg.image.load("apple.jpg") #変更_問２
+    bard_img = pg.image.load("fig/3.png") #変更_問２
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bard_img, [0, 0]) #背景の貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(10)
